25b66306b2bd86c99ccb7e85bd47f3a822a2d191
François Pinard <pinard@iro.umontreal.ca>
2008-01-21 19:51:40 -0500

    Group rebox files into new contrib/rebox/

---
+++ contrib/rebox/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import sys
+sys.path.insert(0, '.')
+from Pymacs import __package__, __version__
+del sys.path[0]
+
+from distutils.core import setup
+
+setup(name=__package__, version=__version__,
+      description="Refilling comment boxes from within Emacs.",
+      author='François Pinard', author_email='pinard@iro.umontreal.ca',
+      url='http://pinard.progiciels-bpi.ca',
+      scripts=['rebox'],
+      packages=['Pymacs.Rebox'])
