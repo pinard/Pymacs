2a964c567c5288771be9c53fa8d788300ce628d2
François Pinard <pinard@iro.umontreal.ca>
2008-02-01 21:28:15 -0500

    test_41 to chase a bug

--- tests/test_41.py
+++ tests/test_41.py
@@ -1,3 +1,16 @@
 # -*- coding: UTF-8 -*-
 
 # Checking if the whole Pymacs works together.
+
+import setup
+from Pymacs import lisp
+
+def test_1():
+    # 2006-06-20 Sebastian Waschik <sebastian.waschik@gmx.de>
+
+    def action():
+        lisp.insert("Test")
+
+    lisp.apply(action, None)
+
+#pymacs_test.interaction = ''
