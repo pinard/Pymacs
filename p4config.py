555fcc1c2a0d7470a09b3e8ef365faab5b8aa73b
François Pinard <pinard@iro.umontreal.ca>
2010-11-05 15:08:50 -0400

    Avoid double signal(SIGINT, ...), the second clobbering the first. Reported by Ali Gholami Rudi.

--- p4config.py
+++ p4config.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 # p4 configuration for Pymacs.
 
+# It has been reported that intercepting all signals (and optionally writing
+# a trace of them, create IO problems within the Pymacs helper itself.  So for
+# now, IO_ERRORS_WITH_SIGNALS is blindly set to True, until I know better.
+# When True, only the Interrupt signal gets monitored.
+
+IO_ERRORS_WITH_SIGNALS = True
+
+# OLD_EXCEPTIONS is True for old Python or Jython versions.
+
 def get_old_exceptions():
     return not isinstance(Exception, type)
 
 OLD_EXCEPTIONS = get_old_exceptions()
 del get_old_exceptions
+
+# PYTHON gets the command name of the Python interpreter.
 
 def get_python():
     import os
@@ -14,12 +25,16 @@ def get_python():
 PYTHON = get_python()
 del get_python
 
+# PYTHON3 is True within Python 3.
+
 def get_python3():
     import sys
     return sys.version_info[0] == 3
 
 PYTHON3 = get_python3()
 del get_python3
+
+# VERSION is the name of the Pymacs version, as declared within setup.py.
 
 def get_version():
     import re
